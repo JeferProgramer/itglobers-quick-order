192dafe475e6c4a820893abc1bab3bac43944c92
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-19 11:08:12 -0500

    feat component quick order ready fpr the wait products back office

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react'
-import { useMutation } from 'react-apollo'
+import React, {useState, useEffect} from 'react'
+import { useMutation, useLazyQuery } from 'react-apollo'
 import UPDATE_CART from '../graphql/updateCart.graphql'
-
+import GET_PRODUCT from "../graphql/getProductBySku.graphql"
 const QuickOrder = () => {
   const [inputText, setInputText] = useState("")
   const [search, setSearch] = useState("")
 
+  const [getProductData, {data: product}] = useLazyQuery(GET_PRODUCT)
   const [addToCart] = useMutation(UPDATE_CART)
 
   const handleChange = (event: any) => {
@@ -14,7 +15,38 @@ const QuickOrder = () => {
   }
 
   const addProductToCart = () => {
+    //en este momento envio el sku de mi input a text al query de graphql para obtenerlo
+    getProductData({
+      variables:{
+        sku: inputText
+      }
+    })
   }
+
+  //cuando la busqueda este activa y cuando el producto llegue
+  useEffect(() => {
+    console.log("El resultado de mi producto es", product, search);
+    if(product){
+      let skuId = parseInt(inputText)
+      console.log("mis datos necesarios", skuId, product)
+     addToCart ({
+        variables: {
+          salesChannel: "1",
+          items: [
+            {
+              id: skuId,
+              quantity: 1,
+              seller: "1"
+            }
+          ]
+        }
+      })
+      .then(() => {
+        window.location.href = "/checkout"
+      })
+    }
+  },[product, search])
+
 
   const searchProduct = (event:any) => {
     event.preventDefault();
